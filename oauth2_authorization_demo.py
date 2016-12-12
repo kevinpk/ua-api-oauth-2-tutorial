5e97defb07d08d90d4e40ae5a77719df9aec3476
Kevin Kelly <kevinpk@gmail.com>
2016-12-12 11:10:24 -0600

    initial commit: working workout POST

--- oauth2_authorization_demo.py
+++ oauth2_authorization_demo.py
@@ -5,6 +5,10 @@ import urlparse
 import webbrowser
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import requests
+import json
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -20,7 +24,7 @@ if CLIENT_ID is None or CLIENT_SECRET is None:
 
 # As a convenience, localhost.mapmyapi.com redirects to localhost.
 redirect_uri = 'http://localhost.mapmyapi.com:12345/callback'
-authorize_url = 'https://www.mapmyfitness.com/v7.0/oauth2/authorize/?' \
+authorize_url = 'https://api.mapmyfitness.com/v7.1/oauth2/authorize/?' \
                 'client_id={0}&response_type=code&redirect_uri={1}'.format(CLIENT_ID, redirect_uri)
 
 print authorize_url
@@ -57,7 +61,7 @@ authorize_code = urlparse.parse_qs(callback_url.query)['code'][0]
 
 print 'Got an authorize code:', authorize_code
 
-access_token_url = 'https://api.mapmyfitness.com/v7.0/oauth2/access_token/'
+access_token_url = 'https://api.mapmyfitness.com/v7.1/oauth2/access_token/'
 access_token_data = {'grant_type': 'authorization_code',
                      'client_id': CLIENT_ID,
                      'client_secret': CLIENT_SECRET,
@@ -81,14 +85,14 @@ except:
     print response.content
 
 # Use the access token to request a resource on behalf of the user
-activity_type_url = 'https://oauth2-api.mapmyapi.com/v7.0/activity_type/'
+activity_type_url = 'https://api.ua.com/v7.1/activity_type/'
 response = requests.get(url=activity_type_url, verify=False,
                         headers={'api-key': CLIENT_ID, 'authorization': 'Bearer %s' % access_token['access_token']})
 
 print 'response: ', response
 
 # Refresh a client's credentials to prevent expiration
-refresh_token_url = 'https://oauth2-api.mapmyapi.com/v7.0/oauth2/access_token/'
+refresh_token_url = 'https://api.ua.com/v7.1/oauth2/access_token/'
 refresh_token_data = {'grant_type': 'refresh_token',
                       'client_id': CLIENT_ID,
                       'client_secret': CLIENT_SECRET,
@@ -119,23 +123,32 @@ response = requests.get(url=activity_type_url, verify=False,
 
 
 # Custom successful request:
-activity_type_url = 'https://oauth2-api.mapmyapi.com/v7.1/workout/?started_after=2014&user=889861'
+activity_type_url = 'https://api.ua.com/v7.1/workout/?started_after=2014&user=889861'
 response = requests.get(url=activity_type_url, verify=False,
                         headers={'api-key': CLIENT_ID, 'authorization': 'Bearer %s' % access_token['access_token']})
 
-#print the json-formatted response:
-response.json()
+##print the json-formatted response:
+#print(response.json())
 
+activity_type_url = 'https://api.ua.com/v7.1/workout/'
 
+testData = json.dumps({'start_datetime': '2016-12-11T20:32:33.768863Z','name': 'Sample Workout JSON','privacy': '/v7.1/privacy_option/1/','aggregates': {'active_time_total': 10.7,'torque_min': 10.7,'power_min': 10.7,'distance_total': 10.7,'cadence_max': 10.7,'speed_max': 10.7,'speed_min': 10.7,'heartrate_min': 100,'cadence_min': 10.7,'speed_avg': 10.7,'torque_max': 10.7,'cadence_avg': 10.7,'power_avg': 10.7,'heartrate_max': 160,'power_max': 10.7,'elapsed_time_total': 10.7,'heartrate_avg': 10.7,'metabolic_energy_total': 10.7,'torque_avg': 10.7},'time_series': {'distance': [[0, 65], [1, 54], [2, 35]],'heartrate': [[0, 100], [1, 120], [2, 110]],'power': [[0, 123], [1, 120], [2, 115]],'timer_stop': [[1, 1], [3, 4], [8, 20]],'torque': [[0, 21], [1, 64], [2, 98]],'steps': [[0, 21], [1, 32], [2, 31]],'position': [[0, {'lat': 107.134, 'lng': 75.234, 'elevation': 5}],[1, {'lat': 107.1356, 'lng': 75.123, 'elevation': 5.2}],[2, {'lat': 107.1365, 'lng': 75.012, 'elevation': 5.3}]],'speed': [[0, 7.5], [1, 8.5], [2, 8.2]],'cadence': [[0, 32], [1, 36], [2, 34]]},'start_locale_timezone': 'US/Central','activity_type': '/v7.1/activity_type/11/'})
 
+#print "dict'start_datetime' = ", testData['start_datetime']
 
+testToken = 'Bearer %s' % access_token['access_token']
 
+testHeaders = {'api-key': CLIENT_ID, 'authorization': testToken}
 
+#print "dict'access_token' = ", testHeaders['authorization']
+
+#postThis = json.dumps({'url': activity_type_url, 'verify': False, 'headers': testHeaders, 'data': testData})
+#
+#print postThis
 #this MIGHT work. Was getting a 500, which means it might be webserver-side issue
 # POST A WORKOUT
-activity_type_url = 'https://oauth2-api.mapmyapi.com/v7.1/workout/'
-response = requests.post(url=activity_type_url, verify=False,
-                        headers={'api-key': CLIENT_ID, 'authorization': 'Bearer %s' % access_token['access_token']},data=json.dumps([{"start_datetime": "2014-02-26T20:32:33.768863Z","aggregates": { "active_time_total": 10.7,"torque_min": 10.7,"power_min": 10.7,"distance_total": 10.7,"cadence_max": 10.7,"speed_max": 10.7,"speed_min": 10.7,"heartrate_min": 100,"cadence_min": 10.7,"speed_avg": 10.7,"torque_max": 10.7,"cadence_avg": 10.7,"power_avg": 10.7,"heartrate_max": 160,"power_max": 10.7,"elapsed_time_total": 10.7,"heartrate_avg": 10.7,"metabolic_energy_total": 10.7,"torque_avg": 10.7}, "start_locale_timezone": "US/Central"}]))
+
+response = requests.post(url = activity_type_url, headers = testHeaders, data = testData)
 
 #print the json-formatted response:
-response.json()
+print(response.json())
